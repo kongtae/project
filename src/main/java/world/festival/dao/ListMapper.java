36af4522ac10a46c63529397051bcea17154aca5
아아이 <show9812@gmail.com>
2019-09-14 17:30:17 +0900

    리스트매퍼 충돌 해결, 마이바티스 해결

--- src/main/java/world/festival/dao/ListMapper.java
+++ src/main/java/world/festival/dao/ListMapper.java
@@ -13,16 +13,8 @@ public interface ListMapper {
 
 	public ListVO listDetail(ListVO vo);
 
-<<<<<<< HEAD
 	public ArrayList<ListVO> selectOne(HashMap<String, String> map);
 
 	public ArrayList<ListVO> selectOne2(HashMap<String, String> map);
-	
-	public ArrayList<ListVO> countryList(String country);
-=======
-	public ArrayList<SearchVO> selectOne(SearchVO vo);
-
-	public ArrayList<SearchVO> selectOne2(HashMap<String, String> map);
->>>>>>> 659866ab54ee2ede57dee8b1f0e35c502fd2d4ce
 	
 }
