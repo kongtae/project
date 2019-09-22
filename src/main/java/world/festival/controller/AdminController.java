77a913efbf9594119e2ac5700e35f6af4b64cb80
Ryung-developer <ez1ryung@gmail.com>
2019-09-22 13:59:03 +0900

    관리자페이지

---
+++ src/main/java/world/festival/controller/AdminController.java
@@ -0,0 +1,22 @@
+package world.festival.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class AdminController {
+	
+	
+	@RequestMapping(value = "adminPage", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "admin/adminPage";
+	}
+}
